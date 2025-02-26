663766ec9ffde845ae0cdc7b719aa2b4ba6d17ee
Twil3akine <twil3akine@gmail.com>
2025-02-27 01:23:03 +0900

    make proto prototype
    - this visual is so bad, so I'll improve it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,59 @@
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
 import './App.css'
+import { ChangeEventHandler, FormEventHandler, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const today = new Date();
+
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [limit, setLimit] = useState<string>(`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`)
+  const [completed, setCompleted] = useState<boolean>(false);
+
+  const handleTitleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setTitle(target.value);
+  }
+
+  const handleDescriptionChange: ChangeEventHandler<HTMLTextAreaElement> = ({ target }) => {
+    setDescription(target.value);
+  }
+
+  const handleLimitChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setLimit(target.value);
+  }
+
+  const handleCompletedChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setCompleted(target.value === "on");
+  }
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = ( event ) => {
+    event.preventDefault();
+
+    alert(
+      `${title}\n${description}\n${limit}\n${completed ? "Yes" : "No"}`
+    )
+  }
 
   return (
     <>
-        <Button onClick={() => setCount((count) => count + (Math.random() >= 0.5 ? 1 : -1))}>count is {count}</Button>
+      <form onSubmit={handleSubmit}>
+        <label>Title:
+          <input type="text" value={title} onChange={handleTitleChange} />
+        </label>
+
+        <label>Description:
+          <textarea value={description} onChange={handleDescriptionChange} />
+        </label>
+
+        <label>Limit:
+          <input type="date" value={limit} onChange={handleLimitChange} />
+        </label>
+
+        <label>Completed:
+          <input type="checkbox" onChange={handleCompletedChange} />
+        </label>
+
+        <input type="submit" value={"Submit"} />
+      </form>
     </>
   )
 }
